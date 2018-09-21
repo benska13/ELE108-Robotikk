2448fc6014a03838e97d8168c1057822a9623ad6
benska13 <benskare@gmail.com>
2018-09-21 13:53:12 +0200

    innlevering

--- Oblig_2_Turtle/Oblig_2_Turtle/Oblig_2_Turtle.py
+++ Oblig_2_Turtle/Oblig_2_Turtle/Oblig_2_Turtle.py
@@ -1,33 +1,47 @@
 import turtle
+import random
 
-
-car = turtle.Turtle()
+# Point that objects go to
 point = turtle.Turtle()
-maxTurnAngle = 1
+# Object:
+maxTurnAngle = 8
+step = 5
 
-car.setpos(00, 00)
-point.setpos(100, 200)
+# Make a list of turtles with random poisson
+turtles = []
+for x in range(10):
+    turtles.append(turtle.Turtle())
+    turtles[x].setpos(random.randrange(-740, 740), random.randrange(-600, 600))
+    turtles[x].setheading(random.randrange(0, 360))
+    turtles[x].clear()
+    turtles[x].speed(10)
 
-car.speed(1)
-car.left(0)
-car.clear()
-point.clear()
 
-while car.distance(point) > 10:
+# Function that turns a turtle
+def go_to_point(tut, point, maxTurnAngle):
+    # Angle between turtle and point
+    angle_ = round(tut.towards(point)) - round(tut.heading())
+    # Sets the angle between -180 and 180 degrees
+    while -180 > angle_ or angle_ > 180:
+        if angle_ < -180:
+            angle_ = angle_ + 360
+        elif angle_ > +180:
+            angle_ = angle_ - 360
 
-    angle_ = car.towards(point) - car.heading()
-    print('Angle to point: ', angle_, ' Car a: ', car.heading(), ' Car to p: ', car.towards(point))
+    # print('Angle to point: ', angle_, ' Car a: ', tut.heading(), ' Car to p: ', round(tut.towards(point)))
 
-    if -360 <= angle_ < -180 or 0 < angle_ < 180:
-        if 0 < angle_ < maxTurnAngle:
-            car.left(angle_)
-        else:
-            car.left(maxTurnAngle)
+    # Turn the turtle
+    if - maxTurnAngle <= angle_ <= maxTurnAngle:
+        tut.setheading(angle_ + tut.heading())
+    elif angle_ < - maxTurnAngle:
+        tut.setheading(tut.heading() - maxTurnAngle)
+    elif angle_ > maxTurnAngle:
+        tut.setheading(tut.heading() + maxTurnAngle)
 
-    elif -180 <= angle_ < 0 or 180 <= angle_ < 360:
-        if 0 > angle_ > - maxTurnAngle:
-            car.right(-angle_)
-        else:
-            car.right(maxTurnAngle)
 
-    car.forward(1)
+# runs trough the list
+while True:
+    for tut in turtles:
+        if tut.distance(point) > 5:
+            go_to_point(tut, point, maxTurnAngle)
+            tut.forward(step)
--- Oblig_2_Turtle/Oblig_2_Turtle_linw/Oblig_2_Turtle_Line.py
+++ Oblig_2_Turtle/Oblig_2_Turtle_linw/Oblig_2_Turtle_Line.py
@@ -5,67 +5,59 @@ import random
 # make a line
 line = turtle.Turtle()
 # (ax+by+c)=0
-a = 1
+a = -1
 b = -1
 c = 0
-for y in range(100):
-    line.setpos((((-b * y) - c) / a), y)
+line.setpos((((-b * -500) - c) / a), -500)
+line.setpos((((-b * 500) - c) / a), 500)
 # line done
 
 maxTurnAngle = 10
 step = 5
 demp = 40.0
 
-# car = turtle.Turtle()
-# car.setpos(-100, 00)
-# car.speed(0)
-# car.right(180)
-# car.clear()
-
 turtles = []
 
-for x in range(1):
+for x in range(10):
     turtles.append(turtle.Turtle())
-    turtles[x].setpos(random.randrange(-400, 400), random.randrange(-300, 300))
+    turtles[x].setpos(random.randrange(-740, 740), random.randrange(-600, 600))
     turtles[x].setheading(random.randrange(0, 360))
     turtles[x].clear()
     turtles[x].speed(10)
 
 
 def follow_line(tut, a, b, c, maxTurnAngle, demp):
-    # Distance to line and nearest point on line
+    # Distance to nearest point on line
     distance_line = round((a * tut.xcor() + b * tut.ycor() + c) / math.sqrt(a * a + b * b))
-    # line_x = ((b * (b * car.xcor() - a * car.ycor()) - a * c) / (a * a + b * b))
-    # line_y = (a * (-b * car.xcor() + a * car.ycor()) - b * c) / (a * a + b * b)
-
+    # Calculate the lines angel. PROBLEM!? angel between -90 and 90 degrees !?!
     angle_line = round(math.degrees(math.atan(- a / b)))
+    # Turns the turtle towards the line. Problem continues
+    angle_corr = round(math.degrees(math.atan(distance_line / demp)))
+    # Try to correct the problem
     if angle_line < 0:
-        angle_line = angle_line + 180
-        distance_line = distance_line
-
-    angle_ = angle_line - round(tut.heading()) - round(math.degrees(math.atan(distance_line / demp)))
-
+        angle_corr = - angle_corr
+    # Calculate the angel to turn
+    angle_ = angle_line - round(tut.heading()) - angle_corr
+    # Sets the angle between -180 and 180 degrees
     while -180 > angle_ or angle_ > 180:
         if angle_ < -180:
             angle_ = angle_ + 360
         elif angle_ > +180:
             angle_ = angle_ - 360
 
-    print('Ang_to_p:', round(angle_), ' A line: ', angle_line, '  Car.h: ', tut.heading(), '  Corr a: ',
-          round(math.degrees(math.atan(distance_line / demp))), '  D line: ', distance_line)
+    # print('Ang_to_p:', round(angle_), ' A line: ', angle_line, '  Car.h: ', tut.heading(), '  Corr a: ',
+    #       round(math.degrees(math.atan(distance_line / demp))), '  D line: ', distance_line)
 
-    if 0 < angle_ <= 180:
-        if 0 < angle_ < maxTurnAngle:
-            tut.left(angle_)
-        else:
-            tut.left(maxTurnAngle)
-    elif -180 <= angle_ < 0:
-        if 0 > angle_ > - maxTurnAngle:
-            tut.right(-angle_)
-        else:
-            tut.right(maxTurnAngle)
+    # Turn the turtle
+    if - maxTurnAngle <= angle_ <= maxTurnAngle:
+        tut.setheading(angle_ + tut.heading())
+    elif angle_ < - maxTurnAngle:
+        tut.setheading(tut.heading() - maxTurnAngle)
+    elif angle_ > maxTurnAngle:
+        tut.setheading(tut.heading() + maxTurnAngle)
 
 
+# Runs trough the list
 while True:
     for tut in turtles:
         follow_line(tut, a, b, c, maxTurnAngle, demp)
